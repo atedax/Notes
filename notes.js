e26d9127c6fccea62a2b194efaf75a6311955470
Ilya Pyannikov <pyannikov2001@mail.ru>
2024-06-15 19:52:35 +0300

    -

--- notes.js
+++ notes.js
@@ -14,20 +14,23 @@ createNoteBtn.addEventListener('click', function() {
     const b = document.createElement('b');
     const p = document.createElement('p');
     const divFooter = document.createElement('div');
-    divFooter.classList.add('note-footer');
     const time = document.createElement('time');
-    time.classList.add('time');
     const divDelete = document.createElement('div');
-    divDelete.classList.add('note-delete');
     const imgDelete = document.createElement('img');
+
+    divFooter.classList.add('note-footer');
+    divDelete.classList.add('note-delete');
+
+    time.classList.add('time');
+
     imgDelete.classList.add('trash-can');
     imgDelete.src = './icon/trash-can.png';
     imgDelete.alt = 'Удалить';
 
     b.textContent = 'Lorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumque';
     p.textContent = 'Lorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumqueLorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumqueLorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumque'
-
     time.textContent = '20 минут(ы) назад';
+
     li.appendChild(b);
     li.appendChild(p);
     li.appendChild(divFooter);
