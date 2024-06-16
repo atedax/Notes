39b1e25e4ac9e0512ad8c67f6eabdd6354f805dc
Ilya Pyannikov <pyannikov2001@mail.ru>
2024-06-17 00:01:17 +0300

    added id to notes

--- notes.js
+++ notes.js
@@ -1,24 +1,27 @@
 const menuBtn = document.getElementById('menu-btn');
 const createNoteBtn = document.getElementById('create-note-btn');
-const sidebar = document.querySelector('.sidebar');
-const content = document.querySelector('.content');
 
 menuBtn.addEventListener('click', function () {
+
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
     sidebar.classList.toggle('sidebar-active');
     content.classList.toggle('content-active');
 })
 
 createNoteBtn.addEventListener('click', function() {
+    const Li = document.createElement('li');
     const ul = document.querySelector('ul');
-    const li = document.createElement('li');
     const b = document.createElement('b');
     const p = document.createElement('p');
     const divFooter = document.createElement('div');
     const time = document.createElement('time');
     const divDelete = document.createElement('div');
     const imgDelete = document.createElement('img');
+    let id = checkUniqueId();
 
     divFooter.classList.add('note-footer');
+
     divDelete.classList.add('note-delete');
 
     time.classList.add('time');
@@ -31,14 +34,42 @@ createNoteBtn.addEventListener('click', function() {
     p.textContent = 'Lorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumqueLorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumqueLorem ipsum dolor sit amet, consecteturm? Ab, accusamus consequatur culpa cumque'
     time.textContent = '20 минут(ы) назад';
 
-    li.appendChild(b);
-    li.appendChild(p);
-    li.appendChild(divFooter);
+    Li.appendChild(b);
+    Li.appendChild(p);
+    Li.appendChild(divFooter);
+    Li.id = 'note-' + id;
 
     divDelete.appendChild(imgDelete);
 
     divFooter.appendChild(time);
     divFooter.appendChild(divDelete);
 
-    ul.appendChild(li);
+    ul.appendChild(Li);
 })
+
+function getRandomId() {
+    return Math.round(Math.random() * 1e10);
+}
+
+function checkUniqueId() {
+    const li = document.getElementsByTagName('li');
+
+    let newId;
+    let uniqueId = true;
+
+    do {
+        newId = String( getRandomId() );
+
+        for (let i = 0; i < li.length; ++i) {
+            if (newId === li[i].id) {
+                uniqueId = false;
+
+                break;
+            }
+        }
+    } while (uniqueId !== true)
+
+    return newId;
+}
+
+
