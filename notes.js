6d4248dc74a329c6ad42de459dff0e4fa5895059
Ilya Pyannikov <pyannikov2001@mail.ru>
2024-07-15 23:46:14 +0300

    added remove objects by id, remove all objects

--- notes.js
+++ notes.js
@@ -1,19 +1,20 @@
 const menuBtn = document.getElementById('menu-btn');
 const createNoteBtn = document.getElementById('create-note-btn');
 const tools = document.querySelector('.tools');
-const saveNotes = document.querySelector('.save-notes');
+const savedNotes = document.querySelector('.saved-notes');
 const blockConfirmBtns = document.querySelector('.block-confirm-buttons');
 const mask = document.querySelector('.mask');
 const confirm = document.querySelector('.confirm');
 const clearAllBtn = document.getElementById('clear-all-btn');
-const db = [];
 
+let dbOfSavedNote = []; // тут будет храниться массив объектов.
 let idCurrentSavedNote; // тут будет храниться id заметки, готовое к уадлению.
-let index = 0;
+let index = 0; // при создании новой заметки - индекс будет увеличиваться. index - это data-value li элемента.
+let dataValue; // тут будет храниться значение элемента li.
 
 function getRandomId() {
     return new Date().getTime();
-}
+} // Возвращет кол-во миллисекунд прошедших с 1 января 1970 года. В моём случае используется для id заметки.
 
 function toolsFunctional(txtElement, event) {
     if (event.target.id === 'bold-block') {
@@ -27,7 +28,7 @@ function toolsFunctional(txtElement, event) {
     } else {
         txtElement.classList.toggle('line-through');
     }
-}
+} // Отвечает за функционал инструментов. (Жирный текст, курсив и т.д.)
 
 function togglesDisplayStatus(status) {
     mask.style.display = status;
@@ -35,8 +36,101 @@ function togglesDisplayStatus(status) {
 } // только для элементов с классом 'mask' и 'confirm'
 
 function textConfirmation(text) {
-    return document.querySelector('.text-in-confirm-body').textContent = text;
-}
+    document.querySelector('.text-in-confirm-body').textContent = text;
+} // текст в модальном окне удаления.
+
+function autocompleteTextInSavedNotes(elementById, tag) {
+    let element = document.getElementById(elementById).value;
+
+    if (element === '') { // Если значение элемента (input или textArea) пустое -->
+        tag.textContent = 'Без названия'; // --> tag(b, p) в сохранённых заметках (savedNotes) вставляем текст заглушку.
+    } else {
+        tag.textContent = element; // --> значение элемента (input или textArea)
+    }
+} // Автоматически вставляет текст в сохранённых заметках. (savedNotes).
+
+function autoCreateSaveNote() {
+    const noteTitle = document.getElementById('note-title');
+    const noteText = document.getElementById('note-text');
+
+    if ( (noteTitle.value !== '' || noteText.value !== '') && savedNotes.firstChild === null ) {
+        createNewNote();
+    }
+} // Автоматически создаём заметку,
+    // если в savedNote нет дочернего элемента (не нажали кнопку 'Создать новую заметку'),
+    // а пользователь начал набирать текст.
+
+function addNoteInDb(id, b, p) {
+    dbOfSavedNote.push({
+        id: 'note-' + id,
+        title: b.textContent,
+        text: p.textContent,
+    });
+
+    // массиву dbOfSavedNote добавляем объект с:
+        // id: хранится id эллемента.
+        // title: хранится заголовок заметки.
+        // text: хранится текст заметки.
+} // Добавляем заметку в database (dbOfSavedNote).
+
+function createNewNote() {
+    if ( !savedNotes.firstChild ) { // Если в savedNotes нет дочернего эллемента -->
+        const createUl = document.createElement('ul'); // Создаём элемент 'ul'.
+
+        savedNotes.appendChild(createUl); // добавляем элементу savedNotes дочерний элемент.
+    }
+
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    const b = document.createElement('b');
+    const p = document.createElement('p');
+    const divFooter = document.createElement('div');
+    const time = document.createElement('time');
+    const divDelete = document.createElement('div');
+    const imgDelete = document.createElement('img');
+
+    let id = getRandomId(); // id заметки.
+
+    divFooter.classList.add('note-footer');
+
+    divDelete.classList.add('note-delete');
+
+    time.classList.add('time');
+
+    imgDelete.classList.add('trash-can');
+    imgDelete.src = './icon/trash-can.png';
+    imgDelete.alt = 'Удалить';
+
+    autocompleteTextInSavedNotes('note-title', b); // автозаполнение тега 'b'.
+    autocompleteTextInSavedNotes('note-text', p); // автозаполнение тега 'p'.
+
+    time.textContent = '20 минут(ы) назад'; // (для себя) сделать время (сколько прошло с момента создания заметки).
+
+    b.classList.add('text-in-saved-notes');
+    p.classList.add('text-in-saved-notes');
+
+    li.appendChild(b);
+    li.appendChild(p);
+    li.appendChild(divFooter);
+    li.id = 'note-' + id;
+    li.classList.add('notes');
+    li.dataset.value = `${index++}`; // присваиваем в атрибут data-value значение index.
+
+    divDelete.appendChild(imgDelete);
+
+    divFooter.appendChild(time);
+    divFooter.appendChild(divDelete);
+
+    ul.appendChild(li);
+
+    addNoteInDb(id, b, p);
+} //
+
+function removeNoteFromDb() {
+
+} // (доделать)
+
+setInterval(autoCreateSaveNote, 100);
 
 
 tools.addEventListener('click', function(event) {
@@ -60,174 +154,54 @@ menuBtn.addEventListener('click', function() {
 })
 
 createNoteBtn.addEventListener('click', function() {
-    if ( !saveNotes.firstChild ) {
-        const createUl = document.createElement('ul');
+    createNewNote();
+})
 
-        saveNotes.appendChild(createUl);
-    }
+savedNotes.addEventListener('click', function(event) {
+    if (event.target.className === 'trash-can') { // Если нажали на 'мусорное ведро' -->
+        textConfirmation('Вы уверены, что хотите удалить эту заметку?'); // --> Указываем текст подтверждения.
 
-    const ul = document.querySelector('ul');
-    const li = document.createElement('li');
-    const b = document.createElement('b');
-    const p = document.createElement('p');
-    const divFooter = document.createElement('div');
-    const time = document.createElement('time');
-    const divDelete = document.createElement('div');
-    const imgDelete = document.createElement('img');
+        blockConfirmBtns.id = 'from-trash-can'; // --> элементу с кнопками подтвеждения удаления, присваиваем id.
 
-    let id = getRandomId();
+        togglesDisplayStatus('flex'); // --> показываем модальное окно удаления.
 
-    divFooter.classList.add('note-footer');
-
-    divDelete.classList.add('note-delete');
-
-    time.classList.add('time');
-
-    imgDelete.classList.add('trash-can');
-    imgDelete.src = './icon/trash-can.png';
-    imgDelete.alt = 'Удалить';
-
-    autocompleteSavedNotes('note-title', b);
-    autocompleteSavedNotes('note-text', p);
-
-    time.textContent = '20 минут(ы) назад';
-
-    b.classList.add('text-in-saved-notes');
-    p.classList.add('text-in-saved-notes');
-
-    li.appendChild(b);
-    li.appendChild(p);
-    li.appendChild(divFooter);
-    li.id = 'note-' + id;
-    li.classList.add('notes');
-    li.dataset.value = index++;
-
-    divDelete.appendChild(imgDelete);
-
-    divFooter.appendChild(time);
-    divFooter.appendChild(divDelete);
-
-    ul.appendChild(li);
-
-
-    db.push({
-       id: id,
-       title: b.textContent,
-       text: p.textContent,
-    });
-}) // использовать функцию создания заметки
-
-saveNotes.addEventListener('click', function(event) {
-    if (event.target.className === 'trash-can') {
-        textConfirmation('Вы уверены, что хотите удалить эту заметку?');
-
-        blockConfirmBtns.id = 'from-trash-can';
-
-        togglesDisplayStatus('flex');
-
-        idCurrentSavedNote = event.target.closest('li').id;
-    }
-
-    if ( event.target.closest('li') ) {
-        console.log( event.target.closest('li').getAttribute('data-value') );
+        idCurrentSavedNote = event.target.closest('li').id; // сохраняем id удаляемого элемента.
+        dataValue = event.target.closest('li').dataset.value; // сохраняем dataValue удаляемого элемента.
     }
 })
 
 blockConfirmBtns.addEventListener('click', function(event) {
-    if (event.target.id === 'del-yes') {
-        if (blockConfirmBtns.id === 'from-trash-can') {
-            document.getElementById(idCurrentSavedNote).remove();
+    if (event.target.id === 'del-yes') { // Если нажали на кнопку 'Да' -->
+        if (blockConfirmBtns.id === 'from-trash-can') { // Если id элемента подтверждения === 'from-trash-can' -->
+            document.getElementById(idCurrentSavedNote).remove(); // --> удаляем элемент по id.
+
+            dbOfSavedNote.splice(dataValue, 1); // из массива dataBase удаляем элемент под индексом dataValue.
+
+            idCurrentSavedNote = undefined; // очищаем переменную.
         }
 
-        if (blockConfirmBtns.id === 'from-clear-all-btn') {
-            document.querySelector('ul').remove();
+        if (blockConfirmBtns.id === 'from-clear-all-btn') { // Если id элемента подтверждения === 'from-clear-all-btn' -->
+            document.querySelector('ul').remove(); // --> удаляем весь тег ul. (соответсвенно все сохранённые заметки удаляются).
+
+            dbOfSavedNote = []; // очищаем массив dataBase.
         }
+
+        togglesDisplayStatus('none'); // убираем модальное окно удаления.
     }
 
-    togglesDisplayStatus('none');
+    if (event.target.id === 'del-no') { // Если нажали кнопку 'Нет' -->
+        idCurrentSavedNote = undefined; // очищаем переменную.
+
+        togglesDisplayStatus('none'); // убираем модальное окно удаления.
+    }
 })
 
 clearAllBtn.addEventListener('click', function() {
-    textConfirmation('Вы уверены, что хотите навсегда удалить все сохраненные заметки?');
+    textConfirmation('Вы уверены, что хотите навсегда удалить все сохраненные заметки?'); // --> Указываем текст подтверждения.
 
-    blockConfirmBtns.id = 'from-clear-all-btn';
+    blockConfirmBtns.id = 'from-clear-all-btn'; // --> элементу с кнопками подтверждения удаления, присваиваем id.
 
-    togglesDisplayStatus('flex');
+    togglesDisplayStatus('flex'); // --> показываем модальное окно удаления.
 })
-
-
-function autocompleteSavedNotes(elementById, tag) {
-    let element = document.getElementById(elementById).value;
-
-    if (element === '') {
-        tag.textContent = 'Без названия';
-    } else {
-        tag.textContent = element;
-    }
-}
-
-function autoCreateSaveNote() {
-    if ( (document.getElementById('note-title').value !== '' ||
-        document.getElementById('note-text').value !== '') && saveNotes.firstChild === null ) {
-        if ( !saveNotes.firstChild ) {
-            const createUl = document.createElement('ul');
-
-            saveNotes.appendChild(createUl);
-        }
-
-        const ul = document.querySelector('ul');
-        const li = document.createElement('li');
-        const b = document.createElement('b');
-        const p = document.createElement('p');
-        const divFooter = document.createElement('div');
-        const time = document.createElement('time');
-        const divDelete = document.createElement('div');
-        const imgDelete = document.createElement('img');
-
-        let id = getRandomId();
-
-        divFooter.classList.add('note-footer');
-
-        divDelete.classList.add('note-delete');
-
-        time.classList.add('time');
-
-        imgDelete.classList.add('trash-can');
-        imgDelete.src = './icon/trash-can.png';
-        imgDelete.alt = 'Удалить';
-
-        autocompleteSavedNotes('note-title', b);
-        autocompleteSavedNotes('note-text', p);
-
-        time.textContent = '20 минут(ы) назад';
-
-        b.classList.add('text-in-saved-notes');
-        p.classList.add('text-in-saved-notes');
-
-        li.appendChild(b);
-        li.appendChild(p);
-        li.appendChild(divFooter);
-        li.id = 'note-' + id;
-        li.classList.add('notes');
-        li.dataset.value = index++;
-
-        divDelete.appendChild(imgDelete);
-
-        divFooter.appendChild(time);
-        divFooter.appendChild(divDelete);
-
-        ul.appendChild(li);
-
-
-        db.push({
-            id: id,
-            title: b.textContent,
-            text: p.textContent,
-        });
-    } // написать функцию создания заметки.
-} // переделать условие
-
-
-setInterval(autoCreateSaveNote, 500);
 
 
